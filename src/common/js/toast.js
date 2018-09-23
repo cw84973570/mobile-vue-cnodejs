c3de353d4601021447edb7e8f3214faf79785b6a
Ned2017 <285833655@qq.com>
2018-09-23 23:25:48 +0800

    Update toast.js

--- src/common/js/toast.js
+++ src/common/js/toast.js
@@ -1,18 +1,15 @@
-
-  /**
-   * 消息消失框
-   */
-export function toast(msg = '', time = 1500) {
-  var toast = document.createElement('div')
+// 消息提示框
+export function toast(msg = '未知', time = 1500) {
+  const toast = document.createElement('div')
   toast.className = 'common-toast common-toast-show'
   toast.innerHTML = msg
   document.body.appendChild(toast)
   toast.style.display = 'block'
   toast.style.margin = `-${toast.offsetHeight / 2}px 0 0 -${toast.offsetWidth / 2}px`
-  var timer = setTimeout(() => {
+  const timer = setTimeout(() => {
     toast.className = 'common-toast common-toast-hide'
     clearTimeout(timer)
-    var timer2 = setTimeout(() => {
+    const timer2 = setTimeout(() => {
       document.body.removeChild(toast)
       clearTimeout(timer2)
     }, 200)
